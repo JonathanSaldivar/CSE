7ef4af33b7cad67b2dd2677ea8846bfd4bb62fec
JonathanSaldivar <44381267+JonathanSaldivar@users.noreply.github.com>
2019-01-08 14:38:19 -0800

    1/8

---
+++ Semester 2 Notes.py
@@ -0,0 +1,17 @@
+print('Hello World')
+# Im a single-line comment
+
+Cars = 5
+driving = True
+
+print('I have %d cars' % Cars)
+
+age = input('How old are you?')
+print('%s?? Really???' % age)
+
+colors = ['red', 'orange', 'blue', 'purple', 'pink']
+colors.append('yellow')
+
+print(colors)
+
+
