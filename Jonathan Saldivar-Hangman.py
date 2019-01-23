bcaaf1d8bf4159d6becba7fac44a0161d2a96d2c
JonathanSaldivar <44381267+JonathanSaldivar@users.noreply.github.com>
2019-01-23 14:35:43 -0800

    1/22

--- Jonathan Saldivar-Hangman.py
+++ Jonathan Saldivar-Hangman.py
@@ -1,5 +1,11 @@
 import random
-words = ['Rocks', 'Loan', 'Clock', 'Hangman', 'Mouse', 'Shoe', 'Avenue', 'Apartment', 'Sleeves', '']
-print(word)
+words = ['Rocks', 'Loan', 'Clock', 'Hangman', 'Mouse', 'Shoe', 'Avenue', 'Apartment', 'Sleeves', 'Genotype']
+print(words)
 letters_guess = 9
 letters_guess_right = []
+guesses_made = []
+random_words = random.choice(words)
+playing = True
+while playing and letters_guess > 0:
+    guess = input('Choose a letter')
+    input()
