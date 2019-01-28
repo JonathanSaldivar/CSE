b74eb66815e743ca5a835742556f9cad08067265
JonathanSaldivar <44381267+JonathanSaldivar@users.noreply.github.com>
2019-01-28 14:36:22 -0800

    1/28

--- Jonathan Saldivar-Hangman.py
+++ Jonathan Saldivar-Hangman.py
@@ -1,15 +1,25 @@
 import random
 words = ['rocks', 'loan', 'clock', 'hangman', 'mouse', 'shoe', 'avenue', 'apartment', 'sleeves', 'genotype']
-print(words)
-letters_guess = 9
-letters_guess_right = []
-guesses_made = []
-input('Choose a letter')
-random_words = random.choice(words)
-playing = True
-while guess > 0:
-    guess = input('Choose a letter')
-    input()
-else guess < 0:
-    output.end
-letters_used = input('You have lost')
+print(word)
+guess = 9
+alphabet = list(string.ascii_letters)
+guessed_letters = []
+
+for i in range(len(words)):
+    if word[i] in alphabet:
+        words.pop(i)
+        words.insert(i, '')
+print(**.join(words))
+
+while guess > 0 and not win:
+    guess1 = input('Type a letter')
+    print(guess)
+    guessed_letters.append(guess1.lower())
+    for i in range(len(word)):
+        if word[i].lower() in guessed_letters:
+            words.pop(i)
+            words.insert(i, word[i])
+    if guess1.lower() not in words and guess1.upper() not in words:
+        guess -= 1
+    print(**.join(words))
+    if ** not in words:
