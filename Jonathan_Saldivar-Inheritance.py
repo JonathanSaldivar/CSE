44163f151b0d95a985c9de2c7473cddceaf60177
JonathanSaldivar <44381267+JonathanSaldivar@users.noreply.github.com>
2019-03-11 14:36:16 -0700

    3/10

--- Jonathan_Saldivar-Inheritance.py
+++ Jonathan_Saldivar-Inheritance.py
@@ -7,6 +7,12 @@ class LaserGun(Item):
     def __init__(self, name):
         super(LaserGun, self).__init__(name)
         self.damage = 150
+        self.usage = 100
+
+    def shoot(self):
+        self.usage -= 1
+        print("You use a bullet.")
+        print('This gun does 150 damage')
 
 
 class BlueLaser(LaserGun):
@@ -31,10 +37,20 @@ class AssaultRifle(Item):
         self.damage = 150
 
 
+class M16(AssaultRifle):
+    def __init__(self):
+        super(M16, self).__init__("M16")
+
+
 class Sword(Item):
     def __init__(self, name):
         super(Sword, self).__init__(name)
         self.damage = 100
+
+
+class LongSword(Sword):
+    def __init__(self):
+        super(LongSword, self).__init__("LongSword")
 
 
 class Hatchet(Item):
@@ -43,10 +59,20 @@ class Hatchet(Item):
         self.damage = 100
 
 
-class Wrench(Item):
+class BattleAxe(Hatchet):
+    def __init__(self):
+        super(BattleAxe, self).__init__("BattleAxe")
+
+
+class Pipe(Item):
     def __init__(self, name):
-        super(Wrench, self).__init__(name)
+        super(Pipe, self).__init__(name)
         self.damage = 75
+
+
+class CopperPipe(Pipe):
+    def __init__(self):
+        super(CopperPipe, self).__init__("Copper Pipe")
 
 
 class Potion(Item):
@@ -55,10 +81,20 @@ class Potion(Item):
         self.heal = 50
 
 
+class ShieldPotion(Potion):
+    def __init__(self):
+        super(ShieldPotion, self).__init__("Shield Potion")
+
+
 class BigPotion(Item):
     def __init__(self, name):
         super(BigPotion, self).__init__(name)
         self.heal = 100
+
+
+class BigShieldPotion(BigPotion):
+    def __init__(self):
+        super(BigShieldPotion, self).__init__("Big Shield Potion")
 
 
 class ChestArmor(Item):
