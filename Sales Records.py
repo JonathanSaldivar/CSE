6b3aa63d03284813ae9467fba80d27da16cfefb6
JonathanSaldivar <44381267+JonathanSaldivar@users.noreply.github.com>
2019-05-08 14:39:21 -0700

    5/8

--- Sales Records.py
+++ Sales Records.py
@@ -1,5 +1,10 @@
 import csv
 
 with open("Sales Records.csv", "r") as csv_file_thing:
-    read = csv.reader(csv_file_thing)
-    
+    reader = csv.reader(csv_file_thing)
+    for row in reader:
+        Profit = row[13]
+        Fruit = row[2]
+        if Fruit == "":
+
+            print(Profit, Fruit)
