7e5e19b039095c3ad7b264e9b2cc40b540d21f0b
JonathanSaldivar <44381267+JonathanSaldivar@users.noreply.github.com>
2019-05-09 14:38:46 -0700

    5/9

--- Sales Records.py
+++ Sales Records.py
@@ -2,9 +2,17 @@ import csv
 
 with open("Sales Records.csv", "r") as csv_file_thing:
     reader = csv.reader(csv_file_thing)
+    total = 0
+
     for row in reader:
         Profit = row[13]
-        Fruit = row[2]
-        if Fruit == "":
-
-            print(Profit, Fruit)
+        Items = row[2]
+        Revenue = row[11]
+        Total_Cost = row[12]
+        Unit_Cost = row[10]
+        Unit_Price = row[9]
+        Unit_Sold = row[8]
+        if Items == "Fruits":
+            # print(Profit, Items, Revenue)
+            total += float(Profit)
+    print(total)
