f9d3966a6e94cf437b3f75e5bef78c41df719745
JonathanSaldivar <44381267+JonathanSaldivar@users.noreply.github.com>
2019-05-13 14:39:46 -0700

    5/13

--- Sales Records.py
+++ Sales Records.py
@@ -1,21 +1,27 @@
 import csv
 
 with open("Sales Records.csv", "r") as csv_file_thing:
+    Profit_For_Fruit = []
+    Profit_For_Meat = []
+    Profit_For_Clothes = []
+    Profit_For_Beverages =[]
     reader = csv.reader(csv_file_thing)
-    total = 0
-
     for row in reader:
         Profit = row[13]
-        Revenue = row[11]
-        Total_Cost = row[12]
-        Unit_Cost = row[10]
-        Unit_Price = row[9]
-        Unit_Sold = row[8]
         Items = row[2]
         if Items == "Fruits":
-            total += float(Profit)
-        if Items == "Clothes":
-            total += float(Profit)
+            Profit_For_Fruit.append(float(Profit))
         if Items == "Meat":
-            total += float(Profit)
-    print(total)
+            Profit_For_Meat.append(float(Profit))
+        if Items == "Clothes":
+            Profit_For_Clothes.append(float(Profit))
+        if Items == "Beverages":
+            Profit_For_Beverages.append(float(Profit))
+Fruits_Total = sum(Profit_For_Fruit)
+Meat_Total = sum(Profit_For_Meat)
+Clothes_Total = sum(Profit_For_Clothes)
+Beverages_Total = sum(Profit_For_Beverages)
+print("Total profit for fruits is %f" % Fruits_Total)
+print("Total profit for meat is %f" % Meat_Total)
+print("Total profit for clothes is %f" % Clothes_Total)
+print("Total profit for beverages is %f" % Beverages_Total)
